47a8de9e4a4613c510e2c2b583935a9568b5b0d7
maria <1230879@isep.ipp.pt>
2025-02-27 16:21:28 +0000

    Adicionei atributos na class Automovel.

--- TESTE/src/main/java/isep/eapli/demo_orm/Automovel.java
+++ TESTE/src/main/java/isep/eapli/demo_orm/Automovel.java
@@ -1,5 +1,11 @@
 package isep.eapli.demo_orm;
 
 public class Automovel {
+    private double km;
+    private String matricula;
 
+    public Automovel(Double km, String matricula){
+        this.km = km;
+        this.matricula = matricula;
+    }
 }
