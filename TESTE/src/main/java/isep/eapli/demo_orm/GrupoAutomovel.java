62a5b473eb3a3a69a48699a8c6b58ee59279adbd
Sofia Pereira Miler <1231010@isep.ipp.pt>
2025-02-27 16:15:19 +0000

    Criação do método setPortas da Classe GrupoAutomovel

--- TESTE/src/main/java/isep/eapli/demo_orm/GrupoAutomovel.java
+++ TESTE/src/main/java/isep/eapli/demo_orm/GrupoAutomovel.java
@@ -1,5 +1,16 @@
 package isep.eapli.demo_orm;
 
 public class GrupoAutomovel {
+    private String nome;
+    private Integer portas;
+    private String classe;
+    public GrupoAutomovel(String nome, Integer portas, String classe){
+        this.nome=nome;
+        this.portas=portas;
+        this.classe=classe;
+    }
 
+    public void setPortas(Integer portas) {
+        this.portas = portas;
+    }
 }
